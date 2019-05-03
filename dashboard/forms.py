9df65a776816cef72e8421256a1b828dd36d3dea
Andrianina <Mahafeno@passion4humanity.com>
2019-05-03 15:33:06 +0300

    Changement : Formulaire ajout offre recruteur

---
+++ dashboard/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+
+from .models import Offre
+
+class OffreForm(forms.ModelForm):
+
+    class Meta:
+        model = Offre
+        widgets = {
+            'intutile': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+            'mission': forms.Textarea(attrs={'class': 'form-control'}),
+            'date_limite': forms.DateInput(attrs={'id': 'datetimepicker1','class': 'form-control'}),
+            # 'date_publication': forms.DateInput(attrs={'id': 'datetimepicker2','class': 'form-control'}),
+            'mot_cle': forms.Select(attrs={'class': 'form-control'}),
+        }
+        
+        fields = ('intutile', 'description','mission','date_limite','mot_cle')
+
+     
+     
+     
+     
+     
+     
